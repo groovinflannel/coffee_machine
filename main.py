ae7c0a749bb2a3d9535464e9c115308a26da6abf
groovinflannel <groovinflannel@gmail.com>
2023-08-07 15:29:43 -0400

    created report() function in main.py, testing with call to report() below function definition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from menu import MENU
 from resources import resources
 
+
 # TODO: 1. Print report of all of the coffee machine's resources
 # TODO: 2. Check that resources are sufficient to make drink order
 # TODO: 3. Check that coins given are more than the cost of the drink
@@ -8,3 +9,11 @@ from resources import resources
 # TODO: 5. Make the drink and print a successful message for the user
 
 # Functions
+def report():
+    print(f"Current resource levels:")
+    print(f"Water: {resources['water']}")
+    print(f"Milk: {resources['milk']}")
+    print(f"Coffee: {resources['coffee']}")
+
+
+report()
