5321f4d4459ea8d47113286acb58a7a67ddb48f8
groovinflannel <groovinflannel@gmail.com>
2023-08-07 14:29:11 -0400

    added 5 TODOs to main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from menu import MENU
 from resources import resources
 
+# TODO: 1. Print report of all of the coffee machine's resources
+# TODO: 2. Check that resources are sufficient to make drink order
+# TODO: 3. Check that coins given are more than the cost of the drink
+# TODO: 4. Return the correct change to the user
+# TODO: 5. Make the drink and print a successful message for the user
+
+# Functions
